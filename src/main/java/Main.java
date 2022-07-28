9bb9ca7afb1db9fcb3eadf91c5144a3ba82f389d
Oscar 8A <oscarochoacerrato@gmail.com>
2022-07-27 20:58:15 -0400

    finished Futures counter function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,39 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.IntStream;
 
 public class Main {
-    public static int countFinishedFutures(List<Future> futures) {
-        // your code here
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        List<Future> futuresList = new ArrayList<>();
+
+        IntStream.rangeClosed(1, 5)
+                .forEach(i -> {
+                    Future<Integer> futureInt = executor.submit(() -> {
+                        try {
+                            Thread.sleep(i * 1000L);
+                            return i;
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                    futuresList.add(futureInt);
+                });
+
+        countFinishedFutures(futuresList);
+        executor.shutdown();
+    }
+
+    public static int countFinishedFutures(List<Future> futures) throws InterruptedException {
+        int count = 0;
+        Thread.sleep(2500);
+        for (Future future : futures) {
+            System.out.println(future.toString());
+            if(future.isDone()) count += 1;
+        }
+        return count;
     }
 }
